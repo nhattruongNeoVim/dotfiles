a4232a8d2a31d9eec7d838765544ed406741e20d
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-10-12 14:15:04 +0700

    update

--- test.sh
+++ test.sh
@@ -28,10 +28,20 @@ write_ask() {
 
 # Start script
 # Set local time
-write_ask "Are you dual booting Windows and Ubuntu? (y/n): "
-read answer
-if [ "$answer" = 'y' ]; then
-    write_start "I will set the local time on Ubuntu to display the correct time on Windows."
-    echo 'timedatectl set-local-rtc 1 --adjust-system-clock'
+# write_ask "Are you dual booting Windows and Ubuntu? (y/n): "
+# read answer
+# if [ "$answer" = 'y' ]; then
+#     write_start "I will set the local time on Ubuntu to display the correct time on Windows."
+#     echo 'timedatectl set-local-rtc 1 --adjust-system-clock'
+# fi
+# write_done
+# Đoạn mã để kiểm tra nếu script đang chạy trong môi trường tương tác
+if [[ -t 0 ]]; then
+  write_ask "Are you dual booting Windows and Ubuntu? (y/n): "
+  read answer
+else
+  answer="y"  # Hoặc có thể sử dụng giá trị mặc định ở đây
 fi
-write_done
+# Đảm bảo script có quyền ghi và đọc từ /dev/tty
+exec </dev/tty
+exec >/dev/tty
