bd41824a14baa7946d72553220a6d9800e41bb11
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-10-12 14:19:42 +0700

    update

--- test.sh
+++ test.sh
@@ -1,47 +1,37 @@
-#!/bin/sh
+# !/bin/bash
 
-echo "   ____   __ __   ____  ______      ______  ____   __ __   ___   ____    ____ "
-echo "  |    \\ |  |  | /    ||      |    |      ||    \\ |  |  | /   \ |    \\  /    |"
-echo "  |  _  ||  |  ||  o  ||      |    |      ||  D  )|  |  ||     ||  _  ||   __|"
-echo "  |  |  ||  _  ||     ||_|  |_|    |_|  |_||    / |  |  ||  O  ||  |  ||  |  |"
-echo "  |  |  ||  |  ||  _  |  |  |        |  |  |    \\ |  :  ||     ||  |  ||  |_ |"
-echo "  |  |  ||  |  ||  |  |  |  |        |  |  |  .  \|     ||     ||  |  ||     |"
-echo "  |__|__||__|__||__|__|  |__|        |__|  |__|\\_| \\__,_| \\___/ |__|__||___,_|"
-echo ""
-echo ""
-echo "-------------------- Script developed by nhattruongNeoVim --------------------"
-echo " -------------- Github: https://github.com/nhattruongNeoVim -----------------"
-echo ""
+echo -e "\e[34m   ____   __ __   ____  ______      ______  ____   __ __   ___   ____    ____ "
+echo -e "  |    \ |  |  | /    ||      |    |      ||    \ |  |  | /   \ |    \  /    |"
+echo -e "  |  _  ||  |  ||  o  ||      |    |      ||  D  )|  |  ||     ||  _  ||   __|"
+echo -e "  |  |  ||  _  ||     ||_|  |_|    |_|  |_||    / |  |  ||  O  ||  |  ||  |  |"
+echo -e "  |  |  ||  |  ||  _  |  |  |        |  |  |    \ |  :  ||     ||  |  ||  |_ |"
+echo -e "  |  |  ||  |  ||  |  |  |  |        |  |  |  .  \|     ||     ||  |  ||     |"
+echo -e "  |__|__||__|__||__|__|  |__|        |__|  |__|\_| \__,_| \___/ |__|__||___,_|"
+echo -e ""
+echo -e ""
+echo -e "-------------------- Script developed by nhattruongNeoVim --------------------"
+echo -e " -------------- Github: https://github.com/nhattruongNeoVim -----------------"
+echo -e ""
 
 # Function util
 write_start() {
-    echo ">> $1"
+    echo -e "\e[32m>> $1\e[0m"
 }
 
 write_done() {
-    echo "Done"
+    echo -e "\e[34mDone\e[0m"
 }
 
 write_ask() {
-    echo -n ">> $1"
+    echo -en "\e[32m>> $1\e[0m"
 }
 
 # Start script
 # Set local time
-# write_ask "Are you dual booting Windows and Ubuntu? (y/n): "
-# read answer
-# if [ "$answer" = 'y' ]; then
-#     write_start "I will set the local time on Ubuntu to display the correct time on Windows."
-#     echo 'timedatectl set-local-rtc 1 --adjust-system-clock'
-# fi
-# write_done
-# Đoạn mã để kiểm tra nếu script đang chạy trong môi trường tương tác
-if [[ -t 0 ]]; then
-  write_ask "Are you dual booting Windows and Ubuntu? (y/n): "
-  read answer
-else
-  answer="y"  # Hoặc có thể sử dụng giá trị mặc định ở đây
+write_ask "Are you dual booting Windows and Ubuntu? (y/n): "
+read answer
+if [ "$answer" = 'y' ]; then
+    write_start "I will set the local time on Ubuntu to display the correct time on Windows."
+    echo 'timedatectl set-local-rtc 1 --adjust-system-clock'
 fi
-# Đảm bảo script có quyền ghi và đọc từ /dev/tty
-exec </dev/tty
-exec >/dev/tty
+write_done
