414cbaed374abd71830f46fa4f4537210fa5333d
Lê Nhật Trường <126251176+nhattruongNeoVim@users.noreply.github.com>
2023-10-20 19:51:39 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -70,7 +70,7 @@ write_done
 # Install packages
 write_start "Installing packages..."
     sudo nala install git neofetch xclip zsh kitty bat ibus-unikey default-jre default-jdk -y
-    sudo nala install fzf make cmake pip tmux cava pip -y
+    sudo nala install fzf make cmake pip tmux cava pip net-tools -y
     sudo add-apt-repository ppa:danielrichter2007/grub-customizer
     sudo nala update && sudo nala upgrade -y
     sudo nala install grub-customizer -y
