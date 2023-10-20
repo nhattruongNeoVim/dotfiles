2daa87e864ca4c0c475ad62d648b6f67951c580c
Lê Nhật Trường <126251176+nhattruongNeoVim@users.noreply.github.com>
2023-10-20 18:20:30 +0700

    Update setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -92,10 +92,9 @@ write_done
 # Install Rust
 write_start "Install Rust..."
     curl --proto '=https' --tlsv1.2 -sSf "https://sh.rustup.rs" | sh
-    zsh
+    bash
     cargo --version
     cargo install lsd --locked
-    bash
 write_done
 
 # Install Nodejs
