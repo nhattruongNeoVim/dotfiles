3d2302b7df4c7387efabc3d09638e1e09013ef89
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-08 19:43:16 +0700

    update

--- home/.cache/wal/colors-wal-st.h
+++ home/.cache/wal/colors-wal-st.h
@@ -1,29 +1,29 @@
 const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#020A12", /* black   */
-  [1] = "#9D2A35", /* red     */
-  [2] = "#C15253", /* green   */
-  [3] = "#E39373", /* yellow  */
-  [4] = "#36738C", /* blue    */
-  [5] = "#B0788F", /* magenta */
-  [6] = "#2EA7B3", /* cyan    */
-  [7] = "#e9c8cb", /* white   */
+  [0] = "#090a0c", /* black   */
+  [1] = "#384358", /* red     */
+  [2] = "#3A4E6A", /* green   */
+  [3] = "#4C4F61", /* yellow  */
+  [4] = "#3D5E85", /* blue    */
+  [5] = "#3F6187", /* magenta */
+  [6] = "#4F6689", /* cyan    */
+  [7] = "#9eaabc", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#a38c8e",  /* black   */
-  [9]  = "#9D2A35",  /* red     */
-  [10] = "#C15253", /* green   */
-  [11] = "#E39373", /* yellow  */
-  [12] = "#36738C", /* blue    */
-  [13] = "#B0788F", /* magenta */
-  [14] = "#2EA7B3", /* cyan    */
-  [15] = "#e9c8cb", /* white   */
+  [8]  = "#6e7683",  /* black   */
+  [9]  = "#384358",  /* red     */
+  [10] = "#3A4E6A", /* green   */
+  [11] = "#4C4F61", /* yellow  */
+  [12] = "#3D5E85", /* blue    */
+  [13] = "#3F6187", /* magenta */
+  [14] = "#4F6689", /* cyan    */
+  [15] = "#9eaabc", /* white   */
 
   /* special colors */
-  [256] = "#020A12", /* background */
-  [257] = "#e9c8cb", /* foreground */
-  [258] = "#e9c8cb",     /* cursor */
+  [256] = "#090a0c", /* background */
+  [257] = "#9eaabc", /* foreground */
+  [258] = "#9eaabc",     /* cursor */
 };
 
 /* Default colors (colorname index)
