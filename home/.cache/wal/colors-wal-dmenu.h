fd420fbc73daa0d0d5a9caad5e482f5acdb87146
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-08 20:21:57 +0700

    update

--- home/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#9eaabc", "#090a0c" },
-	[SchemeSel] = { "#9eaabc", "#384358" },
-	[SchemeOut] = { "#9eaabc", "#4F6689" },
-};
