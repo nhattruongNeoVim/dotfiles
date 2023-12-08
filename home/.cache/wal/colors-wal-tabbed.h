227cc81c51615edf7fc023db7fc26e82ce5e512e
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-08 20:15:26 +0700

    update

--- home/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#090a0c";
-static const char* selfgcolor   = "#9eaabc";
-static const char* normbgcolor  = "#3A4E6A";
-static const char* normfgcolor  = "#9eaabc";
-static const char* urgbgcolor   = "#384358";
-static const char* urgfgcolor   = "#9eaabc";
