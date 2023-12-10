a65b47b02140b67279ebaeed14263cf2ebcfdb92
Lê Nhật Trường <126251176+nhattruongNeoVim@users.noreply.github.com>
2023-12-10 14:32:58 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -67,7 +67,7 @@ write_done
 # Install packages
 write_start "Installing packages..."
     sudo nala install git neofetch xclip zsh kitty bat rofi ibus-unikey default-jdk htop stow -y
-    sudo nala install fzf make cmake aria2 pip tmux cava net-tools python3.10-venv unzip lolcat cpufetch bpytop figlet sl cmatrix trash-cli ranger hollywood -y
+    sudo nala install fzf make cmake aria2 pip tmux cava net-tools unzip lolcat cpufetch bpytop figlet sl cmatrix trash-cli ranger hollywood -y
     sudo add-apt-repository ppa:danielrichter2007/grub-customizer
     sudo nala update && sudo nala upgrade -y
     sudo nala install grub-customizer -y
