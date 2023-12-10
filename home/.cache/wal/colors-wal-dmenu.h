6773e05674c4352a2d27cf24a5dac0435cbd8fae
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-10 16:14:06 +0700

    update

--- home/.cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#e9c8cb", "#020A12" },
-	[SchemeSel] = { "#e9c8cb", "#9D2A35" },
-	[SchemeOut] = { "#e9c8cb", "#2EA7B3" },
-};
--- home/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#020A12";
-static const char* selfgcolor   = "#e9c8cb";
-static const char* normbgcolor  = "#C15253";
-static const char* normfgcolor  = "#e9c8cb";
-static const char* urgbgcolor   = "#9D2A35";
-static const char* urgfgcolor   = "#e9c8cb";
