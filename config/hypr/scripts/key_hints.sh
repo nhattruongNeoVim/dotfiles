c84d7855610418dafeb81cb3291c81ec2e6b038f
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-02-09 09:33:21 +0700

    update

--- config/hypr/scripts/key_hints.sh
+++ config/hypr/scripts/key_hints.sh
@@ -36,37 +36,32 @@ yad --width=$dynamic_width --height=$dynamic_height \
     --column=Description: \
     --column=Command: \
     --timeout-indicator=bottom \
-"ESC" "close this app" "" "=" "SUPER KEY (Windows Key)" "(SUPER KEY)" \
-" enter" "Terminal" "(kitty)" \
-" or  D" "App Launcher" "(rofi)" \
-" T" "Open File Manager" "(Thunar)" \
+" = " "SUPER KEY (Windows Key)" "(SUPER KEY)" \
+" T" "terminal" "(kitty)" \
+" D" "app launcher" "(rofi)" \
+" E" "view keybinds, settings, monitor" "" \
+" R" "reload waybar swaync rofi" "CHECK NOTIFICATION FIRST!!!" \
+" F" "open file manager" "(thunar)" \
 " Q" "close active window" "(not kill)" \
+" V" "clipboard manager" "(cliphist)" \
+" W" "choose wallpaper" "(wallpaper Menu)" \
+" B" "hide / unhide waybar" "waybar" \
+" M" "fullscreen mode 0" "toggles to full screen mode 0" \
+" Shift M" "fullscreen mode 1" "toggles to full screen mode 1" \
 " Shift Q " "closes a specified window" "(window)" \
-" Alt V" "Clipboard Manager" "(cliphist)" \
-" W" "Choose wallpaper" "(Wallpaper Menu)" \
-"CTRL ALT W" "Random wallpaper" "(via swww)" \
-" B" "Hide/UnHide Waybar" "waybar" \
-" CTRL B" "Choose waybar styles" "(waybar styles)" \
-" ALT B" "Choose waybar layout" "(waybar layout)" \
-" ALT R" "Reload Waybar swaync Rofi" "CHECK NOTIFICATION FIRST!!!" \
-" SHIFT N" "Launch Notification Panel" "swaync Notification Center" \
-" Print" "screenshot" "(grim)" \
+" Shift W" "choose waybar styles" "(waybar styles)" \
+" Shift N" "launch notification panel" "swaync notification center" \
 " Shift Print" "screenshot region" "(grim + slurp)" \
 " Shift S" "screenshot region" "(swappy)" \
-"ALT Print" "Screenshot active window" "active window only" \
-"CTRL ALT P" "power-menu" "(wlogout)" \
-"CTRL ALT L" "screen lock" "(swaylock)" \
-"CTRL ALT Del" "Hyprland Exit" "(SAVE YOUR WORK!!!)" \
-" F" "Fullscreen" "Toggles to full screen" \
-" ALT L" "Toggle Dwindle | Master Layout" "Hyprland Layout" \
-" Shift F" "Toggle float" "single window" \
-" ALT F" "Toggle all windows to float" "all windows" \
-" Shift B" "Toggle Blur" "normal or less blur" \
-" SHIFT G" "Gamemode! All animations OFF or ON" "toggle" \
-" H" "Launch this app" "" \
-" E" "View or EDIT Keybinds, Settings, Monitor" "" \
-"" "" "" \
-"More tips:" "https://github.com/JaKooLit/Hyprland-Dots/wiki" ""\
-
-
-
+" Shift F" "toggle float" "single window" \
+" Shift B" "toggle Blur" "normal or less blur" \
+" Shift G" "gamemode! All animations OFF or ON" "toggle" \
+" Print" "screenshot" "(grim)" \
+" Ctrl W" "choose waybar layout" "(waybar layout)" \
+" Alt F" "toggle all windows to float" "all windows" \
+" Escape" "power-menu" "(wlogout)" \
+"Alt Print" "Screenshot active window" "active window only" \
+"Ctrl Alt L" "screen lock" "(swaylock)" \
+"Ctrl Alt W" "random wallpaper" "(via swww)" \
+"Ctrl Alt Del" "hyprland exit" "SAVE YOUR WORK!!!" \
+"Ctrl Space" "toggle dwindle | master layout" "hyprland layout" \
