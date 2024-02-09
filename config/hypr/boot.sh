c7c639b1e918afb6681fbaa784ee08339c294563
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-02-09 21:39:49 +0700

    update

--- config/hypr/boot.sh
+++ config/hypr/boot.sh
@@ -7,7 +7,7 @@
 
 # Variables
 scriptsDir=$HOME/.config/hypr/scripts
-wallpaper=$HOME/Pictures/wallpapers/anime-lanscape.png
+wallpaper=$HOME/Pictures/wallpapers/sunset.jpg
 waybar_style="$HOME/.config/waybar/style/simple [pywal].css"
 kvantum_theme="Tokyo-Night"
 
