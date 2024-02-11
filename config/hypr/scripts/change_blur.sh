63e682c3f3315f6a9d06221639a004084298b87d
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-02-11 14:47:49 +0700

    update change_blur script
    add no blur

--- config/hypr/scripts/change_blur.sh
+++ config/hypr/scripts/change_blur.sh
@@ -5,12 +5,16 @@ notif="$HOME/.config/swaync/images/bell.png"
 
 STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
 
-if [ "${STATE}" == "2" ]; then
+if [ "${STATE}" == "0" ]; then
 	hyprctl keyword decoration:blur:size 2
 	hyprctl keyword decoration:blur:passes 1
 	notify-send -e -u low -i "$notif" "Less blur"
-else
+elif [ "${STATE}" == "1" ]; then
 	hyprctl keyword decoration:blur:size 5
 	hyprctl keyword decoration:blur:passes 2
 	notify-send -e -u low -i "$notif" "Normal blur"
+else
+	hyprctl keyword decoration:blur:size 0
+	hyprctl keyword decoration:blur:passes 0
+	notify-send -e -u low -i "$notif" "No blur"
 fi
