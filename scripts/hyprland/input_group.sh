189413e76e3fb3230f9d57993670a2b68ccf10fd
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-03-25 17:46:38 +0700

    update

--- scripts/hyprland/input_group.sh
+++ scripts/hyprland/input_group.sh
@@ -10,33 +10,24 @@ ORANGE=$(tput setaf 166)
 YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
-while true; do
-    echo "${NOTE} This script will add your user to the 'input' group."
-    echo "${NOTE} Please note that adding yourself to the 'input' group might be necessary for waybar keyboard-state functionality."
+echo "${NOTE} This script will add your user to the 'input' group."
+echo "${NOTE} Please note that adding yourself to the 'input' group might be necessary for waybar keyboard-state functionality."
 
-    if [[ -z $input_group_choid ]]; then
-      read -n1 -rep "${YELLOW}Do you want to proceed? (y/n): ${RESET}" input_group_choid
-    fi
+if gum confirm "${YELLOW}Do you want to proceed? (y/n): ${RESET}"; then
+	# Check if the 'input' group exists
+	if grep -q '^input:' /etc/group; then
+		echo "${OK} 'input' group exists."
+	else
+		echo "${NOTE} 'input' group doesn't exist. Creating 'input' group..."
+		sudo groupadd input
+		echo "'input' group created"
+	fi
 
-    if [[ $input_group_choid == "y" || $input_group_choid == "Y" ]]; then
-        # Check if the 'input' group exists
-        if grep -q '^input:' /etc/group; then
-            echo "${OK} 'input' group exists."
-        else
-            echo "${NOTE} 'input' group doesn't exist. Creating 'input' group..."
-            sudo groupadd input
-            echo "'input' group created"
-        fi
-
-        # Add the user to the input group
-        sudo usermod -aG input "$(whoami)"
-        echo "${OK} User added to the 'input' group. Changes will take effect after you log out and log back in."
-        echo "User added to 'input' group"
-        break
-    elif [[ $input_group_choid == "n" || $input_group_choid == "N" ]]; then
-        echo "${NOTE} No changes made. Exiting the script."
-        break
-    else
-        echo "${ERROR} Invalid choice. Please enter 'y' for yes or 'n' for no."
-    fi
-done
+	# Add the user to the input group
+	sudo usermod -aG input "$(whoami)"
+	echo "${OK} User added to the 'input' group. Changes will take effect after you log out and log back in."
+	echo "User added to 'input' group"
+	break
+else
+	echo "${NOTE} No changes made. Exiting the script."
+fi
