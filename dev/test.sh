e6c9bf3be003ca8e0c32371a8e191add6d3697f7
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-03-25 20:10:13 +0700

    update

---
+++ dev/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# echo "Please choose between the main-release or the rolling-release (development version):"
+# version=$(gum choose "main-release" "rolling-release")
+# if [ "$version" == "main-release" ]; then
+#     echo 'aaa'
+# elif [ "$version" == "rolling-release" ]; then
+#     echo 'bbb'
+# else
+# 	exit 130
+# fi
+# echo ":: Download complete."
+
+# choose=$(gum confirm "How do you want to proceed?" --affirmative "≤ 1080p" --negative "≥ 1440p")
+choose=$(gum choose "≤ 1080p" "≥ 1440p")
+
+if [ "$choose" == "≤ 1080p" ]; then
+    echo "Please"
+elif [ "$choose" == "≥ 1440p" ]; then
+    echo "ok"
+fi
