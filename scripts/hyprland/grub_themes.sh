9fae46d8ce2f8a2542f546f5d01d34e5bac44451
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-03-25 17:42:35 +0700

    update

--- scripts/hyprland/grub_themes.sh
+++ scripts/hyprland/grub_themes.sh
@@ -21,84 +21,74 @@ echo -e "${NOTE} Setting up grub theme."
 # Check file
 if [ ! -f "$grub" ]; then
 	echo "${NOTE} $grub does not exist. Skipping"
-    exit 1
+	exit 1
 fi
 
 # Ask user
-while true; do
-	read -n1 -rep "${NOTE} Do you want to install grub custom theme? (y/n) " answer
-
-	if [[ $answer == "y" || $answer == "Y" ]]; then
-
-		# Check grub themes file
-		cd ~
-		if [ -d grub_themes ]; then
-			rm -rf grub_themes || {
-				printf "%s - Failed to remove old grub themes folder\n" "${ERROR}"
-				exit 1
-			}
-			printf "\n${NOTE} Clone grub_themes. " && git clone https://github.com/nhattruongNeoVim/grub_themes.git --depth 1 || {
-				printf "%s - Failed to cd grub_themes directory\n" "${ERROR}"
-				exit 1
-			}
-		else
-			printf "\n${NOTE} Clone grub_themes. " && git clone https://github.com/nhattruongNeoVim/grub_themes.git --depth 1 || {
-				printf "%s - Failed to clone grub_themes\n" "${ERROR}"
-				exit 1
-			}
-		fi
-
-		printf "\n%.0s" {1..2}
-
-		# Update GRUB_TIMEOUT
-		sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=\"-1\"/" "$grub"
-		sudo sed -i "s/^GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=\"hidden\"/" "$grub"
-		echo "${OK} Disable grub timeouts in $grub"
-
-		# # Check and diable SUBMENU
-		# if grep -q "^.*GRUB_DISABLE_SUBMENU=" "$grub"; then
-		# 	sudo sed -i "s|^.*GRUB_DISABLE_SUBMENU=.*|GRUB_DISABLE_SUBMENU=y|" "$grub"
-		# 	echo "${OK} Disable submenu in $grub"
-		# else
-		# 	echo "GRUB_DISABLE_SUBMENU=y" | sudo tee -a "$grub" >/dev/null
-		# 	echo "${OK} Added GRUB_DISABLE_SUBMENU to $grub"
-		# fi
-
-		# Check and update GRUB_THEME
-		if grep -q "^.*GRUB_THEME=" "$grub"; then
-			sudo sed -i "s|^.*GRUB_THEME=.*|GRUB_THEME=\"$grub_theme\"|" "$grub"
-			echo "${OK} Updated grub themes in $grub"
-		else
-			echo "GRUB_THEME=\"$grub_theme\"" | sudo tee -a "$grub" >/dev/null
-			echo "${OK} Added GRUB_THEME to $grub"
-		fi
-
-		# Check and update GRUB_DISABLE_OS_PROBER
-		if grep -q "^.*GRUB_DISABLE_OS_PROBER=" "$grub"; then
-			sudo sed -i "s|^.*GRUB_DISABLE_OS_PROBER=.*|GRUB_DISABLE_OS_PROBER=false|" "$grub"
-			echo "${OK} Updated os-prober in $grub"
-		else
-			echo "GRUB_DISABLE_OS_PROBER=false" | sudo tee -a "$grub" >/dev/null
-			echo "${OK} Added to GRUB_DISABLE_OS_PROBER in  $grub"
-		fi
-
-		printf "\n%.0s" {1..2}
-
-		# Extract and copy theme
-		tar xzvf grub_themes/themes/"$theme".tar.gz >/dev/null
-		rm -fr grub_themes/themes/"$theme".tar.gz
-		sudo mkdir -p /boot/grub/themes
-		sudo cp -r $theme /boot/grub/themes
-		rm -rf $theme
-		rm -rf grub_themes
-
-		# Regenerate grub config
-		sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-		break
-	elif [[ $answer == "n" || $answer == "N" ]]; then
-		break
+if gum confirm "${NOTE} Do you want to install grub custom theme? (y/n) "; then
+	cd $HOME
+	if [ -d grub_themes ]; then
+		rm -rf grub_themes || {
+			printf "%s - Failed to remove old grub themes folder\n" "${ERROR}"
+			exit 1
+		}
+		printf "\n${NOTE} Clone grub_themes. " && git clone https://github.com/nhattruongNeoVim/grub_themes.git --depth 1 || {
+			printf "%s - Failed to cd grub_themes directory\n" "${ERROR}"
+			exit 1
+		}
 	else
-		echo "${ERROR} Invalid input. Please enter y or n."
+		printf "\n${NOTE} Clone grub_themes. " && git clone https://github.com/nhattruongNeoVim/grub_themes.git --depth 1 || {
+			printf "%s - Failed to clone grub_themes\n" "${ERROR}"
+			exit 1
+		}
 	fi
-done
+
+	printf "\n%.0s" {1..2}
+
+	# Update GRUB_TIMEOUT
+	sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=\"-1\"/" "$grub"
+	sudo sed -i "s/^GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=\"hidden\"/" "$grub"
+	echo "${OK} Disable grub timeouts in $grub"
+
+	# # Check and diable SUBMENU
+	# if grep -q "^.*GRUB_DISABLE_SUBMENU=" "$grub"; then
+	# 	sudo sed -i "s|^.*GRUB_DISABLE_SUBMENU=.*|GRUB_DISABLE_SUBMENU=y|" "$grub"
+	# 	echo "${OK} Disable submenu in $grub"
+	# else
+	# 	echo "GRUB_DISABLE_SUBMENU=y" | sudo tee -a "$grub" >/dev/null
+	# 	echo "${OK} Added GRUB_DISABLE_SUBMENU to $grub"
+	# fi
+
+	# Check and update GRUB_THEME
+	if grep -q "^.*GRUB_THEME=" "$grub"; then
+		sudo sed -i "s|^.*GRUB_THEME=.*|GRUB_THEME=\"$grub_theme\"|" "$grub"
+		echo "${OK} Updated grub themes in $grub"
+	else
+		echo "GRUB_THEME=\"$grub_theme\"" | sudo tee -a "$grub" >/dev/null
+		echo "${OK} Added GRUB_THEME to $grub"
+	fi
+
+	# Check and update GRUB_DISABLE_OS_PROBER
+	if grep -q "^.*GRUB_DISABLE_OS_PROBER=" "$grub"; then
+		sudo sed -i "s|^.*GRUB_DISABLE_OS_PROBER=.*|GRUB_DISABLE_OS_PROBER=false|" "$grub"
+		echo "${OK} Updated os-prober in $grub"
+	else
+		echo "GRUB_DISABLE_OS_PROBER=false" | sudo tee -a "$grub" >/dev/null
+		echo "${OK} Added to GRUB_DISABLE_OS_PROBER in  $grub"
+	fi
+
+	printf "\n%.0s" {1..2}
+
+	# Extract and copy theme
+	tar xzvf grub_themes/themes/"$theme".tar.gz >/dev/null
+	rm -fr grub_themes/themes/"$theme".tar.gz
+	sudo mkdir -p /boot/grub/themes
+	sudo cp -r $theme /boot/grub/themes
+	rm -rf $theme
+	rm -rf grub_themes
+
+	# Regenerate grub config
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+else
+	echo -e "${NOTE} You chose not install grub theme. Skipping..."
+fi
