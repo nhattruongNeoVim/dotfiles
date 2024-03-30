e3cfd8e767680aa5625224343a6783192d56f59b
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-03-30 09:16:24 +0700

    update

--- scripts/wsl.sh
+++ scripts/wsl.sh
@@ -3,9 +3,18 @@
 # source library
 source <(curl -sSL https://is.gd/nhattruongNeoVim_lib)
 
+# start script
 clear
-if hostnamectl | grep -q 'Ubuntu'; then
-    exScriptWsl "ubuntu.sh"
-elif hostnamectl | grep -q 'Arch'; then
-    exScriptWsl "arch.sh"
+
+# check if the system is running under WSL and determine the distribution
+if hostnamectl | grep -q 'Virtualization: wsl'; then
+	if hostnamectl | grep -q 'Ubuntu'; then
+		exScriptWsl "ubuntu.sh"
+	elif hostnamectl | grep -q 'Arch'; then
+		exScriptWsl "arch.sh"
+	else
+		echo "${ERROR} This script is only available on ubuntu or arch distributions."
+	fi
+else
+	echo "${NOTE} This script is only available under Windows Subsystem for Linux (WSL)."
 fi
