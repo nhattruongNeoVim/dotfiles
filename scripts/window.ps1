98c1b20ef385326c1bc9d77586d2ec7ef52e540c
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-04-11 16:07:39 +0700

    update

--- scripts/window.ps1
+++ scripts/window.ps1
@@ -12,17 +12,20 @@ Write-Host "--------------- Github: httpls://github.com/nhhattruongNeoVim ------
 Write-Host ""
 
 # Util function
-function Write-Start {
+function Start {
     param ($msg)
-    Write-Host(">> "+$msg) -ForegroundColor Green
+    Write-Host("-> "+$msg) -ForegroundColor Green
 }
 
-function Write-Done { Write-Host "Done" -ForegroundColor Blue; Write-Host }
+function Done { 
+    Write-Host "Done" -ForegroundColor Blue;
+    Write-Host 
+}
 
 # Start
 Start-Process -Wait powershell -verb runas -ArgumentList "Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0"
 
-Write-Start -msg "Installing scoop..."
+Start -msg "Installing scoop..."
 if (Get-Command scoop -errorAction SilentlyContinue)
 {
     Write-Warning "Scoop already installed"
@@ -31,54 +34,54 @@ else {
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
-Write-Done
+Done
 
-Write-Start -msg "Initializing Scoop..."
+Start -msg "Initializing Scoop..."
     scoop install git
     scoop bucket add extras
     scoop bucket add nerd-fonts
     scoop bucket add java
     scoop update
-Write-Done
+Done
 
-Write-Start -msg "Installing Scoop's packages"
+Start -msg "Installing Scoop's packages"
     scoop install <# apps #> flow-launcher oh-my-posh fzf winrar lsd
     scoop install <# coding #> git neovim neovide vscode gcc nodejs openjdk python postman alacritty make winfetch lazygit
     scoop install <# reauirement for mason_neovim #> unzip wget gzip pwsh
-Write-Done
+Done
 
-Write-Start -msg "Installing TGPT(Chat GTP)..."
+Start -msg "Installing TGPT(Chat GTP)..."
     scoop install https://raw.githubusercontent.com/aandrew-me/tgpt/main/tgpt.json
-Write-Done
+Done
 
-Write-Start -msg "Start config"
+Start -msg "Start config"
 
 # Clone dotfiles
-Write-Start -msg "Clone dotfiles"
+Start -msg "Clone dotfiles"
     cd ~
     git clone https://github.com/nhattruongNeoVim/dotfiles
     cd dotfiles
-Write-Done
+Done
 
 # Config powershell
-Write-Start -msg "Config Powershell"
+Start -msg "Config Powershell"
     New-Item -Path $PROFILE -Type File -Force
     $PROFILEPath = $PROFILE
     $profileContent = Get-Content -Path ".\config\window\powershell\Microsoft.PowerShell_profile.ps1"
     $profileContent | Set-Content -Path $PROFILEPath
-Write-Done
+Done
 
 # Config Alacritty
-Write-Start -msg "Config Alacritty"
+Start -msg "Config Alacritty"
     $DestinationPath = "~\AppData\Roaming\alacritty"
     If (-not (Test-Path $DestinationPath)) {
         New-Item -ItemType Directory -Path $DestinationPath -Force
     }
     Copy-Item ".\config\window\alacritty\alacritty.yml" -Destination $DestinationPath -Force
-Write-Done
+Done
 
 # Config Neovim
-Write-Start -msg "Config Neovim"
+Start -msg "Config Neovim"
     $DestinationPath = "~\AppData\Local"
     If (-not (Test-Path $DestinationPath)) {
         New-Item -ItemType Directory -Path $DestinationPath -Force
@@ -86,15 +89,15 @@ Write-Start -msg "Config Neovim"
     Copy-Item ".\config\window\nvim" -Destination $DestinationPath -Force -Recurse
     pip install pynvim
     npm install neovim -g
-Write-Done
+Done
 
 # Remove dotfiles
-Write-Start -msg "Remove dotfiles"
+Start -msg "Remove dotfiles"
     cd ~
     Remove-Item dotfiles -Recurse -Force
-Write-Done
+Done
 
-Write-Start -msg "Installing choco..."
+Start -msg "Installing choco..."
 if (Get-Command choco -errorAction SilentlyContinue)
 {
     Write-Warning "Choco already installed"
@@ -103,23 +106,23 @@ else {
     Start-Process -Wait powershell -verb runas -ArgumentList "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
     Start-Process -Wait powershell -verb runas -ArgumentList "choco feature enable -n allowGlobalConfirmation"
 }
-Write-Done
+Done
 
-Write-Start -msg "Installing Choco's packages"
+Start -msg "Installing Choco's packages"
     Start-Process -Wait powershell -verb runas -ArgumentList "choco install zalopc internet-download-manager vmwareworkstation" 
     # Start-Process -Wait powershell -verb runas -ArgumentList "choco install steam bluestacks" 
-Write-Done
+Done
 
-Write-Start -msg "Enable Virtualiztion"
+Start -msg "Enable Virtualiztion"
 Start-Process -Wait powershell -verb runas -ArgumentList @"
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -Norestart
     Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -Norestart
     Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -All -Norestart
     Enable-WindowsOptionalFeature -Online -FeatureName Contaniners -All -Norestart
 "@
-Write-Done
+Done
 
-# Write-Start -msg "Installing WSl..."
+# Start -msg "Installing WSl..."
 # if(!(wsl -l -v)){
 #     wsl --install
 #     wsl --update
@@ -128,4 +131,4 @@ Write-Done
 # else {
 #     Write-Warning "Wsl installed"
 # }
-Write-Done
+Done
