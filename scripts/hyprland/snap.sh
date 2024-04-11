4e528c912c1164176eca9b683288f88eccaa877c
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-04-11 17:05:43 +0700

    update

--- scripts/hyprland/snap.sh
+++ scripts/hyprland/snap.sh
@@ -10,28 +10,31 @@ if [ -d snapd ]; then
 	rm -rf snapd
 fi
 
-if command -v snap &>/dev/null; then
-	printf "\n%s - Snap already installed, moving on.\n" "${OK}"
-else
-	printf "\n%s - Snap was NOT located\n" "${NOTE}"
-	printf "\n%s - Installing snap\n" "${NOTE}"
-	git clone https://aur.archlinux.org/snapd.git || {
-		printf "%s - Failed to clone snap from\n" "${ERROR}"
-		exit 1
-	}
-	cd snapd || {
-		printf "%s - Failed to enter snapd directory\n" "${ERROR}"
-		exit 1
-	}
-	makepkg -si --noconfirm || {
-		printf "%s - Failed to install snap\n" "${ERROR}"
-		exit 1
-	}
-	cd ~ && rm -rf snapd || {
-		printf "%s - Failed to remove snapd directory\n" "${ERROR}"
-		exit 1
-	}
-fi
+# if command -v snap &>/dev/null; then
+# 	printf "\n%s - Snap already installed, moving on.\n" "${OK}"
+# else
+# 	printf "\n%s - Snap was NOT located\n" "${NOTE}"
+# 	printf "\n%s - Installing snap\n" "${NOTE}"
+# 	git clone https://aur.archlinux.org/snapd.git || {
+# 		printf "%s - Failed to clone snap from\n" "${ERROR}"
+# 		exit 1
+# 	}
+# 	cd snapd || {
+# 		printf "%s - Failed to enter snapd directory\n" "${ERROR}"
+# 		exit 1
+# 	}
+# 	makepkg -si --noconfirm || {
+# 		printf "%s - Failed to install snap\n" "${ERROR}"
+# 		exit 1
+# 	}
+# 	cd ~ && rm -rf snapd || {
+# 		printf "%s - Failed to remove snapd directory\n" "${ERROR}"
+# 		exit 1
+# 	}
+# fi
+
+printf "\n%s - Installing snap\n" "${NOTE}"
+install_aur_pkg snapd
 
 # Setup snapd before proceeding
 printf "\n%s - Set up snap.... \n" "${NOTE}"
