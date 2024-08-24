f7a5d8bb994601853d0c1a2fefa1287500f5d1eb
Lê Nhật Trường <126251176+nhattruongNeoVim@users.noreply.github.com>
2024-08-24 21:21:28 +0700

    Update ubuntu.sh

--- scripts/wsl/ubuntu.sh
+++ scripts/wsl/ubuntu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # config wsl
 
-# source library
-source <(curl -sSL https://is.gd/nhattruongNeoVim_lib)
-
 # require
 exScriptGnome "boot.sh"
+
+# source library
+source <(curl -sSL https://is.gd/nhattruongNeoVim_lib)
 
 # init
 clear
@@ -45,12 +45,9 @@ NEOVIM="https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.t
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 LAZYGIT="https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 
-# reload package manager
-PKGMN=$(command -v nala || command -v apt)
-
 # add repository
-#printf "\n%s - Add repository .... \n" "${NOTE}"
-#sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+# printf "\n%s - Add repository .... \n" "${NOTE}"
+# sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
 
 # reupdate system
 printf "\n%s - Update system .... \n" "${NOTE}"
@@ -62,7 +59,7 @@ fi
 
 # list packages
 pkgs=(
-    #fastfetch
+    # fastfetch
     build-essential
     python3
     python3-pip
