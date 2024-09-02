217008090e0c3ec1c44a6276c010843e7c699952
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-09-02 10:54:56 +0700

    update

--- scripts/wsl.sh
+++ scripts/wsl.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# source library
+# Source library
 source <(curl -sSL https://is.gd/nhattruongNeoVim_lib) && clear
 
-# Get system information
-sys_info=$(sudo hostnamectl)
-
 # Check if the system is running under WSL
-if echo "$sys_info" | grep -q 'WSL2'; then
-    if echo "$sys_info" | grep -q 'Ubuntu'; then
+if grep -qi 'microsoft' /proc/sys/kernel/osrelease; then
+    # Determine the distribution
+    . /etc/os-release
+    case $ID in
+    ubuntu)
         exScriptWsl "ubuntu.sh"
-    elif echo "$sys_info" | grep -q 'Arch'; then
+        ;;
+    arch)
         exScriptWsl "arch.sh"
-    else
+        ;;
+    *)
         echo "${ERROR} This script is only available on Ubuntu or Arch distributions."
-    fi
+        ;;
+    esac
 else
     echo "${NOTE} This script is only available under Windows Subsystem for Linux (WSL)."
 fi
