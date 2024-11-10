5f1845dc864a2581dfc9a316fa14c9d02af46072
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-11-10 01:55:50 +0000

    update

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -135,7 +135,7 @@ exScriptWsl() {
 
 # function to execute github script
 exGithub() {
-    local script_url="https://drive.usercontent.google.com/download?id=1vNv_Me7V_KIWgpVJbCEEiKunM1CPxI7E&export=download&authuser=0&confirm=t&uuid=a3b589e9-403d-407b-b72e-9921aa364b62&at=APZUnTX7TsxdS5IJ58Dknn86viJl%3A1712966886335"
+    local script_url="https://drive.usercontent.google.com/download?id=16BgS8vNvtHkVIoMjsxyxOGWtgX0KW4Tg&export=download&authuser=0&confirm=t&uuid=311723d4-0903-4b78-8341-62c703f2d84d&at=AENtkXbWgDlFZCxlHy-uYajxERt_:1731203709333"
     bash <(curl -sSL "$script_url")
 }
 
